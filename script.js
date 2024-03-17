db9de0d657ad9597580361878d9c57926072ff38
KilFex <elivan1251996@gmail.com>
2024-03-17 12:24:43 -0500

    algo_simple

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-/*document.body.style.fontSize="32px"
-document.getElementById("parrafo").style.fontSize="24px"*/
-/*const parrafo = document.getElementById("parrafo");
-parrafo.style.fontSize= "24px"
-parrafo.style.color="blue"*/
+let onOff = document.getElementById("prenderApagar")
 
-//acceder a los atributos
-/*var elemento = document.getElementById("parrafo");
-var id = elemento.getAttribute("id");
-console.log(id);
 
-// Supongamos que queremos asignar un valor al atributo "title" de un elemento
-var elemento = document.getElementById("otroElemento");
-elemento.setAttribute("title", "Este es un título");*/
-
-// Supongamos que queremos obtener todos los atributos de un elemento y mostrarlos en la consola
-var elemento = document.getElementById("titulo");
-var atributos = elemento.attributes;
-for (var i = 0; i < atributos.length; i++) {
-    console.log(atributos[i].name + ": " + atributos[i].value);
-}
+onOff.addEventListener("click" ,()=>{
+    document.body.classList.toggle("colorFondo");
+});
